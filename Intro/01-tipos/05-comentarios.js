874ec202be38eb517d9d852373933d46d6c0e592
DiegoDiazMtz <diegodiaz220308@gmail.com>
2024-05-10 15:33:55 -0600

    Funciones

---
+++ Intro/01-tipos/05-comentarios.js
@@ -0,0 +1,7 @@
+/*
+Comentario extra largo
+*/
+
+let number = 42;
+
+console.log(number); //comentario
---
+++ Intro/01-tipos/07-arrays.js
@@ -0,0 +1,11 @@
+let animales = ['chancho','caballo'];
+
+console.log(animales);
+console.log(animales[0]);
+animales[2] = 'perro';
+console.log(animales);
+
+animales[10] = 'gato';
+console.log(animales);
+
+console.log(animales.length);
