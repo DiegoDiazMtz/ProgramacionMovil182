d16ca8dcef3c2e7cd288f744e237649109b12a82
DiegoDiazMtz <diegodiaz220308@gmail.com>
2024-05-20 15:42:00 -0600

    Avance Pract2

---
+++ JS4React/index.js
@@ -0,0 +1,38 @@
+/*
+-- Funcion --
+
+    primer ejemplo de funcion
+    function suma(a,b){
+        return a+b;
+    }
+
+    console.log(suma(2,3));
+
+
+-- Funcion fatArrow -- 
+
+    const suma = (a,b) => {
+        return a+b
+    }
+
+-- Funcion fatArrow simplificada -- 
+
+    const suma = (a,b) => a+b;
+
+
+    console.log(suma(2,3));
+*/
+
+const usuaraio = {
+    nombre: 'Diego',
+    apellidoP: 'Diaz',
+    edad: 20,
+    direccion: {
+        pais: 'Mexico',
+        ciudad: 'Qro',
+        calle: 'Calle 1',
+    },
+    amigos: ['Maya', 'Fer'],
+    status: true,
+    
+}
