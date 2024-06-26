63d8ecc7963c6fb582f4928760202803ed97023b
DiegoDiazMtz <diegodiaz220308@gmail.com>
2024-06-26 14:59:13 -0600

    SectionList

--- hola-mundo/App.js
+++ hola-mundo/App.js
@@ -1,29 +1,47 @@
 // Importaciones 
 import { StatusBar } from 'expo-status-bar';
-import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
+import { Button, FlatList, StyleSheet, Text, View, SectionList } from 'react-native';
 
 //Existen componentes, area donde se va a ejecutar
 export default function App() {
+
+
   return (
 
     <View style={styles.container}>
       
-      <FlatList 
-
-        data={[
-          {key:1,name:'Diego'},
-          {key:2,name:'Maya'},
-          {key:3,name:'Monse'},
-          {key:4,name:'Isay'},
-          {key:5,name:'Pablo'},
-          {key:6,name:'Alan'},
-          {key:7,name:'Victor'},
+      <SectionList
+        sections={[
+          { title:'Grupo A:', 
+            data:[
+              {key:1,name:'Diego'},
+              {key:2,name:'Maya'},
+              {key:3,name:'Monse'},
+              {key:4,name:'Isay'},
+            ]
+          },
+          { title:'Grupo B:', 
+            data:[
+              {key:5,name:'Pablo'},
+              {key:6,name:'Alan'},
+              {key:7,name:'Victor'},
+            ]
+          },
+          { title:'Grupo C:', 
+            data:[
+              {key:8,name:'Isa'},
+              {key:9,name:'Elias'},
+              {key:10,name:'Roman'},
+              {key:11,name:'Juan'},
+            ]
+          },
         ]}
 
-        renderItem= {({item}) => <Text style={styles.item} > {item.name} </Text>} //Para iterar la lista
+        renderItem={({item}) => <Text style={styles.item}> {item.name} </Text>}
+        renderSectionHeader={({section}) => <Text style={styles.section}> {section.title} </Text>}
 
       />
-      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -44,7 +62,16 @@ const styles = StyleSheet.create({
     padding:10,
     fontSize:24,
     height:50,
-    borderColor:'blue',
+    borderColor:'black',
     borderBottomWidth:1
+  },
+  section: {
+    paddingTop:4,
+    paddingBottom:4,
+    fontSize:27,
+    fontWeight:'bold',
+    backgroundColor:'gray',
+    height:50,
+    borderColor:'black',
   },
 });
